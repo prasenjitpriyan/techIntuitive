a943491405a3976751dddba0717a48e1fab85666
Prasenjit Das <prasenjitpriyan@gmail.com>
2024-06-17 08:27:17 +0530

    Copyright page added

---
+++ src/app/copyright/page.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const CopyRightPage = () => {
+  return (
+    <section className="min-h-screen">
+      <div className="m-10 bg-myColors-100">
+        <p>
+          The copyright for the content on the Techintutive blog website belongs
+          to the owner or operator of the website, unless otherwise specified.
+          This includes original articles, images, videos, and any other
+          original content created by the website&apos;s contributors. The
+          copyright notice should be clearly displayed on the website to
+          indicate that the content is protected by copyright law. It&apos;s
+          important to note that copyright protection applies automatically when
+          original content is created and fixed in a tangible form, such as
+          being published on a website. However, it&apos;s also advisable for
+          the website owner to register their copyright with the appropriate
+          authorities to strengthen their legal protections. Additionally, the
+          website&apos;s terms of use should outline how users are permitted to
+          interact with the content, including any restrictions on reproduction,
+          distribution, or other uses of the material. This helps to clarify the
+          rights and responsibilities of both the website owner and its
+          visitors. If you have specific questions about copyright for the
+          Techintutive blog website, it&apos;s best to consult with a legal
+          professional who can provide tailored advice based on the
+          website&apos;s unique circumstances.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default CopyRightPage;
